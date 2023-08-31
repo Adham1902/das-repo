fa71c468b4b2cb4f1d8d098a141458a5e252690f
yahiaislam2008 <yi820690@gmail.com>
2023-08-31 11:28:51 +0200

    new things

---
+++ taskdesign.py
@@ -0,0 +1,41 @@
+import PyQt5.QtWidgets as qtw
+import task14 as window
+#from PyQt5.QtWidgets import QLineEdit
+
+class mainwindow(qtw.QMainWindow,window.Ui_ToDoList):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.tasks = []
+        self.addtask.clicked.connect(self.add_task)
+        self.deletetask.clicked.connect(self.delete_task)
+        self.deleteall.clicked.connect(self.delete_all)
+        self.exit.clicked.connect(self.Exit)        
+    def add_task(self):
+        task = self.addedtask.toPlainText()
+        due_data=self.duetask.toPlainText()
+        self.tasks.append({"task": task, "due_date": due_data, "done": False})
+        self.listoftasks.addItem(task)
+        self.addedtask.clear()
+        self.duetask.clear()
+
+
+    def delete_task(self):
+        selecteditems=self.listoftasks.selectedItems()
+        if selecteditems:
+            selectetask=selecteditems[0].text()
+            self.listoftasks.takeItem(self.listoftasks.row(selecteditems[0]))
+            for taskitem in self.tasks:
+                if taskitem["task"] == selecteditems:
+                    self.tasks.remove(taskitem)
+    def delete_all(self):
+        self.listoftasks.clear()
+        self.tasks=[]
+
+    def Exit(self):
+        win.close()
+        
+app = qtw.QApplication([])
+win=mainwindow()
+win.show()
+app.exec()
